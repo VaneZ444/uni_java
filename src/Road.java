a5d9d3a7cdda07efab6c2af33082b0d0952bde1b
VaneZ444 <bsaaa444@gmail.com>
2024-10-15 02:03:50 +0400

    a slight progress in 1.6.10

--- src/Road.java
+++ src/Road.java
@@ -1,16 +1,20 @@
 public class Road {
     City roadTo;
-    int roadCost;
-    public Road(City roadTo, int roadCost){
+    City roadFrom;
+    int length;
+    public Road(City roadFrom,City roadTo, int roadCost){
+        this.roadFrom = roadFrom;
         this.roadTo = roadTo;
-        this.roadCost = roadCost;
+        this.length = roadCost;
     }
-
+    public boolean isEqualRoad(Road r){
+        return (this.roadTo.equals(r.roadTo)&&this.roadFrom.equals(r.roadFrom));
+    }
     @Override
     public String toString() {
         return "Road{" +
                 "roadTo=" + roadTo +
-                ", roadCost=" + roadCost +
+                ", roadCost=" + length +
                 '}';
     }
 }

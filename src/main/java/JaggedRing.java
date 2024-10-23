421f0548b884b7a3a74830e3a3c3e453082dcee6
VaneZ444 <bsaaa444@gmail.com>
2024-10-23 12:59:34 +0400

    3.1.2 check for 2 point line added

--- src/main/java/JaggedRing.java
+++ src/main/java/JaggedRing.java
@@ -3,6 +3,7 @@ public class JaggedRing extends JaggedLine{
         super(points);
     }
     public double len(){
+        if (linePoints.size() < 3) return super.len();
         Line tempLine = new Line(linePoints.getFirst(),linePoints.getLast());
         return super.len() + tempLine.lineLen();
     }
